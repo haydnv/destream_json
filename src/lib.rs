347e9bf6f106268c332b2cac5344e8f1a1218274
Haydn Vestal <haydnv@gmail.com>
2021-01-13 17:54:16 -0600

    add tests for JSON primitive types

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::str::FromStr;
 
 use async_trait::async_trait;
 use destream::{de, FromStream, Visitor};
-use futures::stream::{Stream, StreamExt};
+use futures::stream::{Fuse, Stream, StreamExt};
 
 const COLON: u8 = b':';
 const COMMA: u8 = b',';
@@ -155,18 +155,15 @@ impl<'a, S: Stream<Item = Vec<u8>> + Send + Unpin + 'a> de::SeqAccess for SeqAcc
 }
 
 pub struct Decoder<S> {
-    source: S,
+    source: Fuse<S>,
     buffer: Vec<u8>,
     numeric: HashSet<u8>,
 }
 
 impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
-    async fn buffer(&mut self) -> Result<(), Error> {
+    async fn buffer(&mut self) {
         if let Some(data) = self.source.next().await {
             self.buffer.extend(data);
-            Ok(())
-        } else {
-            Err(Error::unexpected_end())
         }
     }
 
@@ -175,12 +172,14 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
 
         let mut i = 0;
         loop {
-            while self.buffer.is_empty() {
-                self.buffer().await?;
+            while self.buffer.is_empty() && !self.source.is_done() {
+                self.buffer().await;
             }
 
-            if self.buffer[i] == QUOTE && (i == 0 || self.buffer[i - 1] != ESCAPE) {
+            if i < self.buffer.len() && self.buffer[i] == QUOTE && (i == 0 || self.buffer[i - 1] != ESCAPE) {
                 break;
+            } else if self.source.is_done() {
+                return Err(Error::unexpected_end())
             } else {
                 i += 1;
             }
@@ -194,12 +193,14 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
     async fn buffer_while<F: Fn(u8) -> bool>(&mut self, cond: F) -> Result<Vec<u8>, Error> {
         let mut i = 0;
         loop {
-            while self.buffer.is_empty() {
-                self.buffer().await?;
+            while i >= self.buffer.len() && !self.source.is_done() {
+                self.buffer().await;
             }
 
-            if cond(self.buffer[i]) {
+            if i < self.buffer.len() && cond(self.buffer[i]) {
                 i += 1;
+            } else if self.source.is_done() {
+                return Ok(self.buffer.drain(..).collect());
             } else {
                 break;
             }
@@ -218,15 +219,23 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
             }
 
             i += 1;
-            while i >= self.buffer.len() {
-                self.buffer().await?;
+            while i >= self.buffer.len() && !self.source.is_done() {
+                self.buffer().await;
+            }
+
+            if self.source.is_done() {
+                return de::Decoder::decode_i64(self, visitor).await;
             }
         }
     }
 
     async fn expect_byte(&mut self, byte: u8) -> Result<(), Error> {
-        while self.buffer.is_empty() {
-            self.buffer().await?;
+        while self.buffer.is_empty() && !self.source.is_done() {
+            self.buffer().await;
+        }
+
+        if self.buffer.is_empty() {
+            return Err(Error::unexpected_end());
         }
 
         let next_char = self.buffer.remove(0);
@@ -243,15 +252,17 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
     async fn expect_comma_or(&mut self, byte: u8) -> Result<(), Error> {
         self.expect_whitespace().await?;
 
-        while self.buffer.is_empty() {
-            self.buffer().await?;
+        while self.buffer.is_empty() && !self.source.is_done() {
+            self.buffer().await;
         }
 
-        if self.buffer[0] != byte {
-            self.expect_byte(COMMA).await?;
+        if self.buffer.is_empty() {
+            Err(Error::unexpected_end())
+        } else if self.buffer[0] == byte {
+            Ok(())
+        } else {
+            self.expect_byte(COMMA).await
         }
-
-        Ok(())
     }
 
     async fn expect_whitespace(&mut self) -> Result<(), Error> {
@@ -260,11 +271,13 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
     }
 
     async fn maybe_byte(&mut self, byte: u8) -> Result<bool, Error> {
-        while self.buffer.is_empty() {
-            self.buffer().await?;
+        while self.buffer.is_empty() && !self.source.is_done() {
+            self.buffer().await;
         }
 
-        if self.buffer[0] == byte {
+        if self.buffer.is_empty() {
+            Ok(false)
+        } else if self.buffer[0] == byte {
             self.buffer.remove(0);
             Ok(true)
         } else {
@@ -276,6 +289,8 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> Decoder<S> {
     where
         <N as FromStr>::Err: fmt::Display,
     {
+        self.expect_whitespace().await?;
+
         let numeric = self.numeric.clone();
         let n = self.buffer_while(|b| numeric.contains(&b)).await?;
         let n = String::from_utf8(n).map_err(Error::invalid_utf8)?;
@@ -295,11 +310,15 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> de::Decoder for Decoder<S> {
     type Error = Error;
 
     async fn decode_any<V: Visitor>(&mut self, visitor: V) -> Result<V::Value, Self::Error> {
-        while self.buffer.is_empty() {
-            self.buffer().await?;
+        self.expect_whitespace().await?;
+
+        while self.buffer.is_empty() && !self.source.is_done() {
+            self.buffer().await;
         }
 
-        if self.buffer[0] == QUOTE {
+        if self.buffer.is_empty() {
+            Err(Error::unexpected_end())
+        } else if self.buffer[0] == QUOTE {
             self.decode_string(visitor).await
         } else if self.buffer[0] == MAP_BEGIN {
             self.decode_map(visitor).await
@@ -314,20 +333,24 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> de::Decoder for Decoder<S> {
         } else if self.buffer.len() >= 4 && self.buffer.starts_with(NULL) {
             self.decode_option(visitor).await
         } else {
-            while self.buffer.len() < 4 {
-                self.buffer().await?;
+            while self.buffer.len() < 4 && !self.source.is_done() {
+                self.buffer().await;
             }
 
-            if self.buffer.starts_with(TRUE) {
+            if self.buffer.is_empty() {
+                Err(Error::unexpected_end())
+            } else if self.buffer.starts_with(TRUE) {
                 self.decode_bool(visitor).await
             } else if self.buffer.starts_with(NULL) {
                 self.decode_option(visitor).await
             } else {
-                while self.buffer.len() < 5 {
-                    self.buffer().await?;
+                while self.buffer.len() < 5 && !self.source.is_done() {
+                    self.buffer().await;
                 }
 
-                if self.buffer.starts_with(FALSE) {
+                if self.buffer.is_empty() {
+                    Err(Error::unexpected_end())
+                } else if self.buffer.starts_with(FALSE) {
                     self.decode_bool(visitor).await
                 } else {
                     let s = String::from_utf8(self.buffer[0..5].to_vec())
@@ -342,20 +365,26 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> de::Decoder for Decoder<S> {
     }
 
     async fn decode_bool<V: Visitor>(&mut self, visitor: V) -> Result<V::Value, Self::Error> {
-        while self.buffer.len() < 4 {
-            self.buffer().await?;
+        self.expect_whitespace().await?;
+
+        while self.buffer.len() < 4 && !self.source.is_done() {
+            self.buffer().await;
         }
 
-        if self.buffer.starts_with(TRUE) {
+        if self.buffer.is_empty() {
+            return Err(Error::unexpected_end());
+        } else if self.buffer.starts_with(TRUE) {
             self.buffer.drain(0..4);
             return visitor.visit_bool(true);
         }
 
-        while self.buffer.len() < 5 {
-            self.buffer().await?;
+        while self.buffer.len() < 5 && !self.source.is_done() {
+            self.buffer().await;
         }
 
-        if self.buffer.starts_with(FALSE) {
+        if self.buffer.is_empty() {
+            return Err(Error::unexpected_end());
+        } else if self.buffer.starts_with(FALSE) {
             self.buffer.drain(0..5);
             return visitor.visit_bool(false);
         }
@@ -416,6 +445,8 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> de::Decoder for Decoder<S> {
     }
 
     async fn decode_string<V: Visitor>(&mut self, visitor: V) -> Result<V::Value, Self::Error> {
+        self.expect_whitespace().await?;
+
         let s = self.parse_string().await?;
         visitor.visit_string(s)
     }
@@ -470,10 +501,10 @@ impl<S: Stream<Item = Vec<u8>> + Send + Unpin> de::Decoder for Decoder<S> {
     }
 }
 
-impl<S> From<S> for Decoder<S> {
+impl<S: Stream> From<S> for Decoder<S> {
     fn from(source: S) -> Self {
         Self {
-            source,
+            source: source.fuse(),
             buffer: vec![],
             numeric: NUMERIC.iter().cloned().collect(),
         }
@@ -500,8 +531,26 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_primitives() {
+    async fn test_json_primitives() {
         assert_eq!(decode::<bool>("true").await, true);
         assert_eq!(decode::<bool>("false").await, false);
+
+        assert_eq!(decode::<u8>("1").await, 1);
+        assert_eq!(decode::<u16>(" 2 ").await, 2);
+        assert_eq!(decode::<u32>("4658 ").await, 4658);
+        assert_eq!(decode::<u64>(&2u64.pow(63).to_string()).await, 2u64.pow(63));
+
+        assert_eq!(decode::<i8>("-1").await, -1);
+        assert_eq!(decode::<i16>("\t\n-32").await, -32);
+        assert_eq!(decode::<i32>("53\t").await, 53);
+        assert_eq!(decode::<i64>(&(-2i64).pow(63).to_string()).await, (-2i64).pow(63));
+
+        assert_eq!(decode::<f32>("2e2").await, 2e2);
+        assert_eq!(decode::<f32>("-2e-3").await, -2e-3);
+        assert_eq!(decode::<f64>("3.14").await, 3.14);
+        assert_eq!(decode::<f32>("-1.414e4").await, -1.414e4);
+
+        assert_eq!(decode::<String>("\"hello world\"").await, "hello world".to_string());
+        assert_eq!(decode::<String>("\t\r\n\" hello world \"").await, " hello world ".to_string());
     }
 }
